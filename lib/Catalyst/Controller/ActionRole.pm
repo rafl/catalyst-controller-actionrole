507e327326deb4e8d004d5dfeb784490d31385df
Florian Ragwitz <rafl@debian.org>
2009-02-20 08:44:26 +0100

    Add some docs.

--- lib/Catalyst/Controller/ActionRole.pm
+++ lib/Catalyst/Controller/ActionRole.pm
@@ -1,4 +1,5 @@
 package Catalyst::Controller::ActionRole;
+# ABSTRACT: Apply roles to action instances
 
 use Moose;
 use Class::MOP;
@@ -9,6 +10,35 @@ use MooseX::Types::Moose qw/Str/;
 use namespace::clean -except => 'meta';
 
 extends 'Catalyst::Controller';
+
+=head1 SYNOPSIS
+
+    package MyApp::Controller::Foo;
+
+    use parent qw/Catalyst::Controller::ActionRole/;
+
+    sub bar : Local Does('Moo') { ... }
+
+=head1 DESCRIPTION
+
+This module allows to apply roles to the C<Catalyst::Action>s for different
+controller methods.
+
+For that a C<Does> attribute is provided. That attribute takes an argument,
+that determines the role, which is going to be applied. If that argument is
+prefixed with C<+>, it is assumed to be the full name of the role. If it's
+prefixed with C<~>, the name of your application followed by
+C<::Action::Role::> is prepended. If it isn't prefixed with C<+> or C<~>,
+the value of the C<_action_role_prefix> attribute will be prepended.
+
+=attr _action_role_prefix
+
+This attribute stores a string that is going to be prepended to all role names
+if they aren't prefixed with C<+> or C<~>. It's built using the
+C<_build__action_role_prefix> method by appending C<::Role::> to the
+controllers C<_action_class>.
+
+=cut
 
 has _action_role_prefix => (
     is      => 'ro',
