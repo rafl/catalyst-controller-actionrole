8f223d107de9be243a9588e8e79e9f4dce1a0ed0
Florian Ragwitz <rafl@debian.org>
2009-04-01 16:29:27 +0200

    Don't depend on parent.pm for the tests.

--- t/lib/Catalyst/Action/TestActionClass.pm
+++ t/lib/Catalyst/Action/TestActionClass.pm
@@ -1,9 +1,15 @@
 package Catalyst::Action::TestActionClass;
-use parent 'Catalyst::Action';
+
+use Moose;
+use namespace::clean -except => 'meta';
+
+extends 'Catalyst::Action';
 
 sub execute {
     my ( $self, $controller, $c ) = @_;
     $c->response->body(__PACKAGE__);
 };
+
+__PACKAGE__->meta->make_immutable;
 
 1;
