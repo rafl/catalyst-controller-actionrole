b5a205a987683812b7e999274a51fa4d275fbb0f
t0m <bobtfish@bobtfish.net>
2009-07-13 00:59:39 +0100

    Unneeded

--- t/lib/TestAppREST/Controller/ActionClass.pm
+++
@@ -1,8 +0,0 @@
-package TestAppTEST::Controller::ActionClass;
-use strict; use warnings;
-use Moose;
-BEGIN { extends 'Catalyst::Controller::ActionRole'; }
-
-sub one : Local ActionClass('REST') {}
-
-1;
